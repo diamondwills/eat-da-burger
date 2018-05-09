0fc162b555b821383a359672b253f0b9c27f6182
Diamond Wills <diamondwills@icloud.com>
2018-05-09 12:26:43 -0400

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO burgers (burger_name) VALUES ("Beef Burger");
 INSERT INTO burgers (burger_name) VALUES ("Ostrich Burger");
 INSERT INTO burgers (burger_name) VALUES ("Buffalo Burger");
